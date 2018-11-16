18d7b0271adc6218a083c64900cbcd816e0522e8
AurorePaladin <aurorepaladin@gmail.com>
2018-11-17 05:32:46 +0800

    mogoose基础

--- dbs/models/person.js
+++ dbs/models/person.js
@@ -4,4 +4,4 @@ let personSchema =new mongoose.Schema({
     name: String,
     age: Number
 })
-export default mongoose.model('Person',personSchema)
+module.exports = mongoose.model('Person',personSchema)
